2f576153c794fb4b39e29afd57db054bb7aadcea
Tuan Anh <williamnguyen8888@gmail.com>
2020-12-16 09:01:05 +0700

    add edit product admin

--- src/main/java/com/controllers/Admin/ProductAdminController.java
+++ src/main/java/com/controllers/Admin/ProductAdminController.java
@@ -12,12 +12,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Transactional
 @Controller
 public class ProductAdminController {
     @Autowired
     private IProductService iProductService;
     @Autowired
     private ICategoryService iCategoryService;
+    @PersistenceContext
+    private EntityManager entityManager;
     @GetMapping("/admin/product")
     public ModelAndView productAdmin(){
         ModelAndView modelAndView = new ModelAndView("admin/html/resultListProduct");
@@ -37,6 +44,11 @@ public class ProductAdminController {
     @GetMapping("/admin/deleteProduct")
     public String deleteProduct(@RequestParam int idProductDelete){
         iProductService.delete(idProductDelete);
+        return "redirect:/admin/product";
+    }
+    @PostMapping("/admin/editProduct")
+    public String updateProduct(@ModelAttribute("productNew") ProductEntity productEntity){
+        entityManager.merge(productEntity);
         return "redirect:/admin/product";
     }
 }
